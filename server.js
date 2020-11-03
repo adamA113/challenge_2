9fd2e9c2ec89749444b3adf9fd1821cf891ee76f
Yasir Tobbileh <yasur96@gmail.com>
2020-11-03 19:07:46 +0200

    trying to convert json to csv and store it in a text file

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 var express = require('express');
 var app = express();
+const fs = require('fs');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static('client'));
+app.use(express.static(__dirname + '/client'));
 
 app.post('/csv', function (req, res) {
-    console.log(req.body)
+    console.log(req)
+    var jsonData = req.body;
+    var storeData = []; 
+    fs.readFile('/samples/test', (err, fileData)=>{
+        if (err) {
+            console.log(err);
+        } 
+        else {
+            
+             
+        }
+    })
+
+    fs.writeFile('/samples/test', jsonData, (err, fileData) => {}
+        
+
+
+    res.redirect("/");
     res.end();
 });
 
-app.get('/', function (req, res) {
-    res.end();
+app.get('/csv', function (req, res) {
+    // res.redirect("/");
 });
 
 app.listen(3000, () => {
