6b4f35c179dfba7f37458f547bcec8239e51fa75
Yasir Tobbileh <yasur96@gmail.com>
2020-11-03 19:51:03 +0200

    creating text file and store csv

--- server.js
+++ server.js
@@ -8,29 +8,48 @@ app.use(express.json());
 app.use(express.static(__dirname + '/client'));
 
 app.post('/csv', function (req, res) {
-    console.log(req)
+    // console.log(req)
     var jsonData = req.body;
+    var storeGeneralKeys = []; 
     var storeData = []; 
-    fs.readFile('/samples/test', (err, fileData)=>{
+
+    //// assumed that the data in sales_report.json is the data arrived to the serer/////
+    // loop through the data and store the general keys in the storeGeneralKeys array
+    for (let key in jsonData) {     
+        storeGeneralKeys.push(key);
+
+        // loop through the data and store the values of each child object in the storeData array
+        if (key === "children") {    
+            for (let i = 0; i < jsonData["children"].length; i++){
+                for (let j = 0; j < storeGeneralKeys.length; j++) {
+                    storeData.push(jsonData["children"][i].storeGeneralKeys[j])
+                }
+            }
+        break; 
+        }
+     }
+    // console.log(storeGeneralKeys)
+
+    //creating a text file to store the generalkeys in the firstline
+    fs.writeFile('/samples/test', storeGeneralKeys, (err,fileData)=>{
         if (err) {
             console.log(err);
         } 
-        else {
-            
-             
+    });
+    
+    // store the data in the nextlines
+    fs.writeFile('/samples/test', jsonData, (err, fileData) => {
+        if (err) {
+            console.log(err);
         }
     })
-
-    fs.writeFile('/samples/test', jsonData, (err, fileData) => {}
-        
-
-
-    res.redirect("/");
+    
+    res.redirect("/csv");
     res.end();
 });
 
 app.get('/csv', function (req, res) {
-    // res.redirect("/");
+    res.render("/");
 });
 
 app.listen(3000, () => {
