c208627a4c52317b71e89a5e9bf688c44a899b23
Yasir Tobbileh <yasur96@gmail.com>
2020-11-03 14:37:48 +0200

    start implementing the local server

--- client/app.js
+++ client/app.js
@@ -1,2 +1,7 @@
-
+document.readyState(()=>{
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        // ... more event handler code here
+    });
+});
 
--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+var express = require('express');
+var app = express();
+
+// app.use(express.static('client');
+
+var ip = '127.0.0.1';
+var port = 3000;
 
 
+app.post('/csv', function (req, res) {
+    
+    res.send();
+});
+
+app.get('/', function (req, res) {
+    res.send();
+});
